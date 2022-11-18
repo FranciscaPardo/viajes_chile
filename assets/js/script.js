23ca9b57c41635a83e2222d574298822a1262bd9
Francisca Pardo <francisca.pardo1@gmail.com>
2022-11-18 14:49:41 -0300

    js y tooltips

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,17 @@
-window.addEventListener('scroll', function() {
+
+var nav = document.querySelector("nav");
+window.addEventListener("scroll", function() {
     if (this.window.pageYOffset >100) {
-        nav.classList.add('bg-dark', 'shadow');
+        nav.classList.add("bg-dark");
     } else {
-        nav.classList.remove('bg-dark', 'shadow');
+        nav.classList.remove("bg-dark");
     }
 });
+
+$(".btn-info").click (function(){
+    alert("mensaje enviado")
+});
+
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
