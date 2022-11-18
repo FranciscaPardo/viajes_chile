cab3da512607307b0163a00fd7b03fa692de20de
Francisca Pardo <francisca.pardo1@gmail.com>
2022-11-18 08:14:57 -0300

    corrección tamaño tarjetas

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+window.addEventListener('scroll', function() {
+    if (this.window.pageYOffset >100) {
+        nav.classList.add('bg-dark', 'shadow');
+    } else {
+        nav.classList.remove('bg-dark', 'shadow');
+    }
+});
